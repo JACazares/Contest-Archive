0245c559f342933440a178922067264d47103093
Jose Angel Cazares <josecazares5305@gmail.com>
2017-07-01 19:05:57 -0500

    Update and rename COCI/Go.cpp to COCI/November 2016/Go.cpp

--- COCI/November 2016/Go.cpp
+++ COCI/November 2016/Go.cpp
@@ -3,28 +3,23 @@ using namespace std;
 int Num_Pokemons, Required[70], Food[70], Evolutions, MaxEvolutions, Evolved;
 string Pokemon[70], MaxPokemon;
 
-int main()
-{
+int main() {
 	cin >> Num_Pokemons;
 	for(int i=0; i<Num_Pokemons; i++)
 		cin >> Pokemon[i] >> Required[i] >> Food[i];
-	for(int i=0; i<Num_Pokemons; i++)
-	{
-		while(Food[i] >= Required[i])
-		{
+	for(int i=0; i<Num_Pokemons; i++) {
+		while(Food[i] >= Required[i]) {
 			Food[i]-= Required[i];
 			Food[i]+= 2;
 			Evolutions++;
 			Evolved++;
 		}
-		if(Evolutions > MaxEvolutions)
-		{
+		if(Evolutions > MaxEvolutions) {
 			MaxEvolutions = Evolutions;
 			MaxPokemon = Pokemon[i];
 		}
 		Evolutions = 0;
 	}
-	cout << Evolved << "\n";
-	cout << MaxPokemon << "\n";
+	cout << Evolved << "\n" << MaxPokemon << "\n";
 	return 0;
 }
